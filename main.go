04d066e69bd5dfd33ad9f66736c9387319789d7e
Chao Xu <xuchao@google.com>
2016-10-30 18:46:21 -0700

    workaround

--- main.go
+++ main.go
@@ -24,38 +24,86 @@ type ExampleSpec struct {
 }
 
 type Example struct {
-	runtime.TypeMeta `json:",inline"`
-	api.ObjectMeta   `json:"metadata"`
+	unversioned.TypeMeta `json:",inline"`
+	// Problem 1:
+	// If ObjectMeta is embedded, because CodecEncodeSelf() is defined for
+	// ObjectMeta in
+	// https://github.com/kubernetes/client-go/blob/v1.5.0/1.5/pkg/api/types.generated.go,
+	// Example will inherit the CodecEncodeSelf(), and ugorji won't call
+	// Example.UnmarshalJSON won't be called.
+	Metadata api.ObjectMeta `json:"metadata"`
 
 	Spec ExampleSpec `json:"spec"`
 }
 
 type ExampleList struct {
+	//TypeMeta unversioned.TypeMeta `json:",inline"`
+	//Metadata unversioned.ListMeta `json:"metadata"`
 	runtime.TypeMeta     `json:",inline"`
 	unversioned.ListMeta `json:"metadata"`
 
 	Items []Example `json:"items"`
 }
 
-func (e *Example) UnmarshalText(data []byte) error {
-	return json.Unmarshal(data, e)
-}
+// Problem 2:
+// Looks like ugorji has bug when using UnmarhsalText. Always get error:
+// json: expect char '"' but got char '{'. So I have to workaround and use
+// UnmarshalJSON.
 
-func (e *Example) MarshalText() ([]byte, error) {
-	return json.Marshal(e)
-}
+// func (e *Example) UnmarshalText(data []byte) error {
+// 	fmt.Println("CHAO: calling Unmarshal Example")
+// 	return json.Unmarshal(data, e)
+// }
+//
+// func (e *Example) MarshalText() ([]byte, error) {
+// 	return json.Marshal(e)
+// }
 
 func (e *Example) GetObjectKind() unversioned.ObjectKind {
 	return &e.TypeMeta
 }
 
-func (el *ExampleList) UnmarshalText(data []byte) error {
-	return json.Unmarshal(data, el)
+type ExampleList2 ExampleList
+
+func (el *ExampleList) UnmarshalJSON(data []byte) error {
+	fmt.Println("CHAO: calling UnmarshalJSON ExampleList")
+	fmt.Printf("CHAO: data=%s\n", data)
+	tmp := ExampleList2{}
+	// Workaround: has to call json.Unmarshal on ExampleList2, otherwise it will
+	// be an endless loop.
+	err := json.Unmarshal(data, &tmp)
+	if err != nil {
+		return err
+	}
+	tmp2 := ExampleList(tmp)
+	*el = tmp2
+	return nil
 }
 
-func (el *ExampleList) MarshalText() ([]byte, error) {
-	return json.Marshal(el)
+type Example2 Example
+
+func (el *Example) UnmarshalJSON(data []byte) error {
+	fmt.Println("CHAO: calling UnmarshalJSON Example")
+	fmt.Printf("CHAO: data=%s\n", data)
+	tmp := Example2{}
+	err := json.Unmarshal(data, &tmp)
+	if err != nil {
+		return err
+	}
+	tmp2 := Example(tmp)
+	*el = tmp2
+	return nil
 }
+
+// func (el *ExampleList) UnmarshalText(data []byte) error {
+// 	fmt.Println("CHAO: calling UnmarshalText ExampleList")
+// 	fmt.Printf("CHAO: data=%s\n", data)
+// 	return json.Unmarshal(data, el)
+// }
+//
+// func (el *ExampleList) MarshalText() ([]byte, error) {
+// 	return json.Marshal(el)
+// }
 
 func (el *ExampleList) GetObjectKind() unversioned.ObjectKind {
 	return &el.TypeMeta
@@ -117,6 +165,7 @@ func buildClientFromFlags(kubeconfig string) (*rest.RESTClient, error) {
 	}
 	config.APIPath = "/apis"
 	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: api.Codecs}
+	config.ContentType = runtime.ContentTypeJSON
 
 	return rest.RESTClientFor(config)
 }
